5e13994d092e954cb49384661911b742ee133201
taylorJ <taylor.ji719@gmail.com>
2022-12-31 15:52:28 -0800

    completed simple adding calculator

---
+++ main.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+name = input("What is your name? ")
+print("hello " + name) #String Concatenation
+
+birth_year = input("Enter your birth year: ")
+age = 2022 - int(birth_year)
+print(age)
+
+#simple calculator logic
+firstInput = input("Enter first number: ")
+secondInput = input("Enter second number: ")
+result = float(firstInput) + float(secondInput)
+print(result)
+
+
+
+
