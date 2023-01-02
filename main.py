3a3cf94d73b818f012e571693e8bb5b8b5681c5c
taylorJ <taylor.ji719@gmail.com>
2023-01-01 22:33:27 -0800

    complete basic python syntax

--- main.py
+++ main.py
@@ -33,19 +33,42 @@ else:
 
 
 #weight converter
-user_weight = input("Enter your weight: ")
-weight_measure = input("(K)g or (L)bs: ")
-upper_user_input = weight_measure.upper()
+# user_weight = input("Enter your weight: ")
+# weight_measure = input("(K)g or (L)bs: ")
+# upper_user_input = weight_measure.upper()
+#
+# if upper_user_input == 'L':
+#     measure_kg = float(user_weight) * 0.453592
+#     print("user weight in found is: " + str(measure_kg))
+# elif upper_user_input == 'K':
+#     measure_lbs = float(user_weight) * 2.20462
+#     print("user weight in found is: " + str(measure_lbs))
+# else:
+#     print("Please check your inputs")
 
-if upper_user_input == 'L':
-    measure_kg = float(user_weight) * 2.20462
-    print("user weight in found is: " + str(measure_kg))
-elif upper_user_input == 'K':
-    measure_lbs = float(user_weight) * 0.453592
-    print("user weight in found is: " + str(measure_lbs))
-else:
-    print("Please check your inputs")
+i = 1
+value = 0
+while (i < 5):
+    print(i * '*')
+    i += 1
 
+names = ["John", "James", "Mosh", "Sam", "Mary"]
+#for each
+for name in names:
+    print(name)
+
+print(len(names))
+
+#include 5, the last element
+ran = range(5, 10, 2) # from five adding two
+print(ran)
+
+#for loop that exclude the last element, because it begins at 0
+for num in ran:
+    print(num)
+
+#tuples, once created, it is immutable
+numbers = (1,2,3,4)
 
 
 
